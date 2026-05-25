b5bd8e63f845fcbddcf137fd0cc65f54449ca16f
GatoCoder282 <diegomvaldez19@gmail.com>
2026-05-24 23:05:43 -0400

    caso basico test agregado

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,5 +1,9 @@
 import calculadora_de_cadenas from './calculadora';
 
+test('Calculadora de cadenas: "" debe ser igual a 0', () => {
+    expect(calculadora_de_cadenas("")).toBe(0);
+});
+
 test('Calculadora de cadenas: "123" debe ser igual a 6', () => {
     expect(calculadora_de_cadenas("123")).toBe(6);
 });
