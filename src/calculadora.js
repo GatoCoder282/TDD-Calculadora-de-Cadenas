ef1519ac5eeea9d801eb0a554a88ecbfcb6e6bc7
GatoCoder282 <diegomvaldez19@gmail.com>
2026-05-24 23:23:36 -0400

    caso de uso arreglado

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,9 +1,22 @@
 function calculadora_de_cadenas(cadena, separador) {
+    if (cadena === '') {
+        return 0;
+    }
+
+    const separadores = [',', '-'];
+
+    if (separador) {
+        separadores.push(separador);
+    }
+
+    const patronSeparadores = new RegExp(separadores.map((valor) => valor.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|'));
+    const numeros = cadena.split(patronSeparadores);
     let resultado = 0;
-    const numeros = cadena.split(separador);
 
     for (let i = 0; i < numeros.length; i++) {
-        resultado += Number(numeros[i]);
+        for (let j = 0; j < numeros[i].length; j++) {
+            resultado += Number(numeros[i][j]);
+        }
     }
 
     return resultado;
