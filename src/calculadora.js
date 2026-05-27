2c9effbfef108e28514b795ef80aa5bdf6662105
GatoCoder282 <diegomvaldez19@gmail.com>
2026-05-27 18:29:17 -0400

    arreglo de codigo

--- src/calculadora.js
+++ src/calculadora.js
@@ -3,6 +3,16 @@ function calculadora_de_cadenas(cadena, separador) {
         return 0;
     }
 
+    if (separador === '') {
+        let resultado = 0;
+
+        for (let i = 0; i < cadena.length; i++) {
+            resultado += Number(cadena[i]);
+        }
+
+        return resultado;
+    }
+
     const separadores = [',', '-'];
 
     if (separador) {
@@ -10,16 +20,10 @@ function calculadora_de_cadenas(cadena, separador) {
     }
 
     const patronSeparadores = new RegExp(separadores.map((valor) => valor.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|'));
-    const numeros = cadena.split(patronSeparadores);
-    let resultado = 0;
 
-    for (let i = 0; i < numeros.length; i++) {
-        for (let j = 0; j < numeros[i].length; j++) {
-            resultado += Number(numeros[i][j]);
-        }
-    }
-
-    return resultado;
+    return cadena
+        .split(patronSeparadores)
+        .reduce((resultado, numero) => resultado + Number(numero), 0);
 }
 
 export default calculadora_de_cadenas;
