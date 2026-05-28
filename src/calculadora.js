5cd079b951823904bbdb016cf944545e0fd32722
GatoCoder282 <diegomvaldez19@gmail.com>
2026-05-27 21:55:43 -0400

    refactor: mejor legibilidad

--- src/calculadora.js
+++ src/calculadora.js
@@ -3,20 +3,32 @@ function calculadora_de_cadenas(cadena) {
         return 0;
     }
 
-    const escapeRegExp = (valor) => valor.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const escapeRegularExpressions = (valor) => valor
+    .replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    
     const separadores = [',', '-'];
     let contenido = cadena;
 
     const encabezado = cadena.match(/^\/\/((?:\[[^\]]+\])+)[ ]*(.*)$/);
 
     if (encabezado) {
-        const delimitadoresPersonalizados = [...encabezado[1].matchAll(/\[([^\]]+)\]/g)].map((coincidencia) => coincidencia[1]);
-        separadores.push(...delimitadoresPersonalizados);
+        const delimitadoresPersonalizados = [...encabezado[1]
+        .matchAll(/\[([^\]]+)\]/g)]
+        .map((coincidencia) => coincidencia[1]);
+
+        separadores
+        .push(...delimitadoresPersonalizados);
+
         contenido = encabezado[2];
     }
 
-    const patronSeparadores = new RegExp(separadores.map(escapeRegExp).join('|'));
-    const partes = contenido.split(patronSeparadores).filter((parte) => parte !== '');
+    const patronSeparadores = new RegExp(separadores
+        .map(escapeRegularExpressions)
+        .join('|'));
+
+    const partes = contenido
+    .split(patronSeparadores)
+    .filter((parte) => parte !== '');
 
     if (!encabezado && partes.length === 1 && /^[\d\s]+$/.test(contenido)) {
         return (contenido.match(/\d/g) || []).reduce((resultado, digito) => resultado + Number(digito), 0);
