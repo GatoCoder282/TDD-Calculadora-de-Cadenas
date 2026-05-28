4b18fe4e1309f5979fb6b3044fd0be834603aedc
GatoCoder282 <diegomvaldez19@gmail.com>
2026-05-27 21:36:48 -0400

    fix: formato de delimitador en funcion

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,29 +1,37 @@
-function calculadora_de_cadenas(cadena, separador) {
+function calculadora_de_cadenas(cadena) {
     if (cadena === '') {
         return 0;
     }
 
-    if (separador === '') {
-        let resultado = 0;
-
-        for (let i = 0; i < cadena.length; i++) {
-            resultado += Number(cadena[i]);
-        }
-
-        return resultado;
-    }
-
+    const escapeRegExp = (valor) => valor.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     const separadores = [',', '-'];
+    let contenido = cadena;
 
-    if (separador) {
-        separadores.push(separador);
+    const encabezado = cadena.match(/^\/\/((?:\[[^\]]+\])+)[ ]*(.*)$/);
+
+    if (encabezado) {
+        const delimitadoresPersonalizados = [...encabezado[1].matchAll(/\[([^\]]+)\]/g)].map((coincidencia) => coincidencia[1]);
+        separadores.push(...delimitadoresPersonalizados);
+        contenido = encabezado[2];
     }
 
-    const patronSeparadores = new RegExp(separadores.map((valor) => valor.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|'));
+    const patronSeparadores = new RegExp(separadores.map(escapeRegExp).join('|'));
+    const partes = contenido.split(patronSeparadores).filter((parte) => parte !== '');
 
-    return cadena
-        .split(patronSeparadores)
-        .reduce((resultado, numero) => resultado + Number(numero), 0);
+    if (!encabezado && partes.length === 1 && /^[\d\s]+$/.test(contenido)) {
+        return (contenido.match(/\d/g) || []).reduce((resultado, digito) => resultado + Number(digito), 0);
+    }
+
+    return partes
+        .reduce((resultado, numero) => {
+            const valor = Number(numero.trim());
+
+            if (Number.isNaN(valor) || valor > 1000) {
+                return resultado;
+            }
+
+            return resultado + valor;
+        }, 0);
 }
 
 export default calculadora_de_cadenas;
